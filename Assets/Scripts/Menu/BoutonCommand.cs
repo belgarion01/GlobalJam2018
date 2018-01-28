d8efacdea4e0f461aad1c137865a09ea9fc20dde
Unknown <hugo.raguin@gmail.com>
2018-01-28 12:46:23 +0100

    DebutMenu

---
+++ Assets/Scripts/Menu/BoutonCommand.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoutonCommand : MonoBehaviour {
+
+	public Camera cam;
+
+	private bool hover = false;
+	private bool change  = false;
+
+	public Sprite noir;
+	public Sprite rouge;
+	public Sprite blanc;
+
+	private SpriteRenderer spriter;
+
+	void Start () {
+		spriter = GetComponent<SpriteRenderer> ();
+	}
+	
+
+	void Update () {
+
+		if (!change) {
+			if (hover) {
+				spriter.sprite = noir;
+				if (Input.GetMouseButtonDown (0)) {
+					spriter.sprite = rouge;
+					change = true;
+					Invoke ("RetourPrincipal", 0.1f);
+				}
+			} else {
+				spriter.sprite = blanc;
+			}
+		}
+
+	}
+
+	void RetourPrincipal()
+	{
+		cam.transform.position = new Vector3 (0, 0, -1f);
+		change = false;
+		spriter.sprite = blanc;
+	}
+
+	void OnMouseEnter()
+	{
+		hover = true;
+		Debug.Log ("ddd");
+	}
+
+	void OnMouseExit()
+	{
+		hover = false;
+	}
+}
---
+++ Assets/Scripts/Menu/VraiBoutonCommande.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VraiBoutonCommande : MonoBehaviour {
+
+	public Camera cam;
+
+	private bool hover = false;
+
+	void Start () {
+	}
+
+
+	void Update () {
+			if (hover) {
+				if (Input.GetMouseButtonDown (0)) {
+					Invoke ("RetourPrincipal", 0.1f);
+				}
+			}
+
+	}
+
+	void RetourPrincipal()
+	{
+		cam.transform.position = new Vector3 (20.5f, 0f, -1f);
+	}
+
+	void OnMouseEnter()
+	{
+		hover = true;
+	}
+
+	void OnMouseExit()
+	{
+		hover = false;
+	}
+}
